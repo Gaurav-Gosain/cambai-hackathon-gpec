164e2c721e77bb9fe4e8c1ae2938211cfd4321a3
Gaurav Gosain <itsgauravgosain@gmail.com>
2024-05-17 14:02:17 -0700

    Update pocketbase.ts

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -1,7 +1,7 @@
 import PocketBase from "pocketbase";
 
-// const pb = new PocketBase(process.env.NEXT_PUBLIC_PB_URL);
-const pb = new PocketBase(process.env.NEXT_PUBLIC_PB_URL_LOCAL);
+const pb = new PocketBase(process.env.NEXT_PUBLIC_PB_URL);
+// const pb = new PocketBase(process.env.NEXT_PUBLIC_PB_URL_LOCAL);
 
 if (process.env.NODE_ENV === "development") pb.autoCancellation(false);
 
